20d4e5f13962b9019c0942f144293e8034b7fa26
GiriAakula <38908324+GiriAakula@users.noreply.github.com>
2022-06-19 10:30:37 +0000

    before zipping

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 const axios = require('axios').default;
 const fs = require('fs');
-var crypto = require('crypto')
+var crypto = require('crypto');
+const {google} = require("googleapis");
+const moment = require("moment");
 
-async function getUser() {
+async function main() {
     try {
         const response = await axios.get('https://jntuaresults.ac.in/');
         writeToFile(response.data);
         const hash = await checksumFile("sha1", "./response.html")
-        console.log('hash', hash)
         deleteFile()
+        console.log('hash', hash)
+        addToSheet(hash)
     } catch (error) {
         console.error(error);
     }
 }
 
-getUser();
+main();
+
+async function addToSheet(hash){
+    const auth = new google.auth.GoogleAuth({
+        keyFile:"credentials.json",
+        scopes:"https://www.googleapis.com/auth/spreadsheets"
+    });
+
+    const client = await auth.getClient();
+
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const spreadsheetId = "1WMMV6CLzwFMTnHCThU8-0Q2cRQcw-MXFRS8hM5O6-mE";
+
+    await googleSheets.spreadsheets.values.append({
+        auth,
+        spreadsheetId,
+        range: "Sheet1!A:B",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: [
+            [moment().utcOffset(330).format('DD-MM-YYYY, HH:mm:ss'), hash],
+        ],
+        },
+    });
+}
+
 function writeToFile(data) {
     fs.writeFileSync('./response.html', data);
 }
